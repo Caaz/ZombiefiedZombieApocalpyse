009b18681890685e5925252a557317b02bbc8802
Daniel Caaz <dcaazy@gmail.com>
2022-01-26 00:13:57 -0800

    Add storyteller, and repeating event!

---
+++ Source/StorytellerCompProperties_HordeWave.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zombiefied
+{
+    internal class StorytellerCompProperties_HordeWave : StorytellerCompProperties
+	{
+		public StorytellerCompProperties_HordeWave()
+		{
+			this.compClass = typeof(StorytellerComp_HordeWave);
+		}
+	}
+}
---
+++ Source/StorytellerComp_HordeWave.cs
@@ -0,0 +1,31 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace Zombiefied
+{
+    internal class StorytellerComp_HordeWave : StorytellerComp
+	{
+		protected StorytellerCompProperties_HordeWave Props
+		{
+			get
+			{
+				return (StorytellerCompProperties_HordeWave)this.props;
+			}
+		}
+		private bool EventReady
+		{
+			get
+			{
+				return (Find.TickManager.TicksGame / 1000 - 30) % (60 * 1) == 0;
+			}
+		}
+		public override IEnumerable<FiringIncident> MakeIntervalIncidents(IIncidentTarget target)
+		{
+			if (!EventReady) yield break;
+			Log.Message("Creating event!");
+			IncidentParms parms = this.GenerateParms(IncidentCategoryDefOf.ThreatBig, target);
+			yield return new FiringIncident(IncidentDef.Named("ZombieHorde"), this, parms);
+		}
+    }
+}
